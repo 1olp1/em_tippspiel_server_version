2e5a5c3c220458dac58a0c65fb595abfa4a62a71
1olp1 <oliver.b.haeussler@gmail.com>
2024-06-22 03:55:24 +0200

    update

--- database_init.py
+++ database_init.py
@@ -1,13 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from models import Base
 
-# Replace with the path to your database
+# Use an absolute path to the SQLite database
 DATABASE_URL = 'sqlite:////tippspiel.db'
-
 engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind=engine)
-session = Session()
 
-# Create all tables
 Base.metadata.create_all(engine)
